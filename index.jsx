b3ad9ca48bbf39606d150506dbb1f50d0ed4df29
Arman Karimi <mrevid2@gmail.com>
2023-08-23 07:24:42 +0430

    moved share icons to a separate component
    + format & indent

--- index.jsx
+++ index.jsx
@@ -1,15 +1,17 @@
-const useState = React.useState
+const useState = React.useState;
+
+const ShareIcons = () => (
+	<div className="flex items-center space-x-4 pl-4">
+		<img src="./images/icon-facebook.svg" alt="facebook" />
+		<img src="./images/icon-twitter.svg" alt="twitter" />
+		<img src="./images/icon-pinterest.svg" alt="pinterest" />
+	</div>
+);
 
 const Share = ({ toggleShareView }) => (
-	<div div className="px-8 py-[14px] rounded-b-[inherit] h-full bg-blue-grayish-very-dark flex items-center" >
-		<h3 className="tracking-[0.3rem] font-normal text-blue-grayish">
-			SHARE
-		</h3>
-		<div className="flex items-center space-x-4 pl-4">
-			<img src="./images/icon-facebook.svg" alt="facebook" />
-			<img src="./images/icon-twitter.svg" alt="twitter" />
-			<img src="./images/icon-pinterest.svg" alt="pinterest" />
-		</div>
+	<div className="px-8 py-[14px] rounded-b-[inherit] h-full bg-blue-grayish-very-dark flex items-center">
+		<h3 className="tracking-[0.3rem] font-normal text-blue-grayish">SHARE</h3>
+		<ShareIcons />
 		<button
 			className="ml-auto bg-blue-unsaturated-dark flex justify-center items-center rounded-full w-8 h-8"
 			onClick={toggleShareView}
@@ -21,8 +23,8 @@ const Share = ({ toggleShareView }) => (
 				/>
 			</svg>
 		</button>
-	</div >
-)
+	</div>
+);
 
 const Profile = ({ toggleShareView }) => (
 	<div className="px-8 py-[14px] rounded-b-[inherit] h-full flex items-center">
@@ -47,7 +49,7 @@ const Profile = ({ toggleShareView }) => (
 			</svg>
 		</button>
 	</div>
-)
+);
 
 const Footer = () => {
 	const [isActive, setIsActive] = useState(false);
@@ -58,14 +60,14 @@ const Footer = () => {
 
 	return (
 		<footer className="rounded-b-[inherit] h-16">
-			{
-				isActive
-					? <Share toggleShareView={toggleShareView} />
-					: <Profile toggleShareView={toggleShareView} />
-			}
+			{isActive ? (
+				<Share toggleShareView={toggleShareView} />
+			) : (
+				<Profile toggleShareView={toggleShareView} />
+			)}
 		</footer>
-	)
-}
+	);
+};
 
 const Content = () => (
 	<article className="px-8 pt-8 pb-5">
@@ -74,12 +76,12 @@ const Content = () => (
 			furniture in your home
 		</h2>
 		<p className="text-blue-unsaturated-dark">
-			Ever been in a room and felt like something was missing? Perhaps it
-			felt slightly bare and uninviting. I've got some simple tips to help
-			you make any room feel complete.
+			Ever been in a room and felt like something was missing? Perhaps it felt
+			slightly bare and uninviting. I've got some simple tips to help you make
+			any room feel complete.
 		</p>
 	</article>
-)
+);
 
 const Card = () => (
 	<div className="flex flex-col justify-center rounded-xl bg-white">
@@ -93,6 +95,6 @@ const Card = () => (
 			<Footer />
 		</section>
 	</div>
-)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Card />);
